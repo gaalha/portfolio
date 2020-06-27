fe6b7c57a2da35fa5d8b4b5877f23597cd6ef4e0
Edgar Mejia <edgar13155@gmail.com>
2020-06-27 03:09:22 -0600

    Added more styled-components to card component, added overlay and blur to cards.

--- src/components/card.js
+++ src/components/card.js
@@ -2,24 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
-import BackgroundImage from 'gatsby-background-image'
+import Img from 'gatsby-image'
 
 const Card = ({
   title,
   subtitle,
   path,
   coverImage,
-  author
+  author,
 }) => (
   <Link to={path}>
-    <BackgroundImage fluid={coverImage.childImageSharp.fluid} className="sexy-card mx-auto">
+    <Container className="mx-auto">
+      <Image fluid={coverImage.childImageSharp.fluid} />
+
       <Content>
         <Title>{title}</Title>
         <Subtitle>
           {subtitle} {author && <>— Written by {author}</>}
         </Subtitle>
       </Content>
-    </BackgroundImage>
+    </Container>
   </Link>
 )
 
@@ -33,25 +35,61 @@ Card.propTypes = {
 
 export default Card
 
+const Image = styled(Img)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  filter: blur(1.5px);
+`
+
+const Container = styled.div`
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  overflow: hidden;
+  margin: 20px 10px;
+  box-shadow: 0px 0px 14px 1px rgba(0, 0, 0, .25), 0px 0px 29px 0px rgba(0, 0, 0, .22);
+  cursor: pointer;
+  transform-origin: center;
+  transition: filter 200ms linear, transform 200ms linear;
+  position: relative;
+
+  &:hover {
+    transform: scale(1.05) translateZ(0);
+    transform-origin: center;
+    transition: filter 200ms linear, transform 200ms linear;
+  }
+`
+
 const Content = styled.div`
+  width: 100%;
+  height: 100%;
   left: 0;
   position: absolute;
   top: 0;
-  padding: 24px;
+  padding: 30px 24px;
+  background-color: #08070738;
+  &:hover {
+    //transition: background-color 0.3s ease;
+    //background-color: transparent;
+    ${Image} {
+      filter: none;
+    }
+  }
 `
 
 const Title = styled.h1`
   color: white;
   font-size: 30px;
   font-weight: bold;
-  margin-left: 20px;
+  margin: 10px 0 0 20px;
   width: 200px;
-  text-shadow: 2px 2px 2px #000;
 `
 
 const Subtitle = styled.p`
   color: white;
-  text-shadow: 2px 2px 2px #000;
-  margin-left: 20px;
+  margin: 0 0 0 20px;
 `
 
