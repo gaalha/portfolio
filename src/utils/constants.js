52e13fb1e25a7da5ff6581cb3b49e0dc089ee115
Edgar Mejia <edgar13155@gmail.com>
2020-10-07 17:21:18 -0600

    Add playlists to redirects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const URL_GITHUB = '/github'
-export const URL_STACKOVERFLOW = '/stack'
+export const URL_STACKOVERFLOW = '/stackoverflow'
 export const URL_TWITTER = '/twitter'
 export const URL_LINKEDIN = '/linkedin'
 export const MY_EMAIL = 'contact@edgarmejia.com'
